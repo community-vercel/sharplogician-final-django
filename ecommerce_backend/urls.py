bb3af2692cff687191da8cc965e2bf7edda6bbe0
community-vercel <community-vercel>
2025-04-25 07:27:51 -0700

    sh

--- ecommerce_backend/urls.py
+++ ecommerce_backend/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     # path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     path('api/admin/', admin.site.urls),
     path('api/', include('inara.urls')),
-    path("api/auth/", include("dj_rest_auth.urls")),  # endpoints provided by dj-rest-auth
+    path("api/api/auth/", include("dj_rest_auth.urls")),  # endpoints provided by dj-rest-auth
     # path("api/social/login/",GoogleLoginView.as_view(), name = "google"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
